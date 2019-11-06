bace837a431463a63ff2d9520123d142e008e8d1
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-11-06 20:41:28 +0700

    Setup Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
+import Dashboard from './screens/Dashboard';
+import LoginScreen from './screens/LoginScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  console.log(process.env.REACT_APP_API_BASE_URL);
-  return <div>Hello</div>;
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <LoginScreen />
+        </Route>
+        <Route path="/products">
+          <ProductScreen />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductScreen = () => {
+  return <div>Ini adalah halaman managemen produk</div>;
+}
+
+export default ProductScreen;
