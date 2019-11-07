c62d9d931da910ecf06cd4f601ef637b1ad11217
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-11-07 23:35:36 +0700

    Apply Grid to CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-import CardItem from './CardItem';
+import CardItem from "./CardItem";
+import { Grid } from "@material-ui/core";
 
 const CardList = ({ items }) => {
-  return items.map(item => {
-    return <CardItem item={item} key={item.id} />;
-  });
+  return (
+    <Grid container spacing={3}>
+      {items.map(item => {
+        return (
+          <Grid item xs={4}>
+            <CardItem item={item} key={item.id} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default CardList;
